98b358cd08ee8a23d54668d3606a35e50d33abcc
snowman <eth.snowman@gmail.com>
2025-02-12 11:35:25 -0300

    fix-errors

--- app/core/config.py
+++ app/core/config.py
@@ -8,7 +8,7 @@ class Settings(BaseSettings):
     SOLANA_RPC_URL = os.getenv("SOLANA_RPC_URL")
     EMAIL_USER = os.getenv("EMAIL_USER")
     EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
-    SECRET_KEY: str = "your-secret-key"
+    SECRET_KEY: str = "f53c4dbb372badaea3c4b3070d060e4f69ac1c924e28a46b8d790fc028fd6c76"
     ALGORITHM: str = "HS256"
 
 settings = Settings()
