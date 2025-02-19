7826cb527788c068c7b5fd921ee472612526de6b
snowman <eth.snowman@gmail.com>
2025-02-19 18:05:37 -0300

    remove-temp-endpoints

--- app/api/routes/trades.py
+++ app/api/routes/trades.py
@@ -6,11 +6,3 @@ router = APIRouter()
 @router.get("/check-arbitrage")
 def arbitrage(price_a: float, price_b: float):
     return check_arbitrage_opportunity(price_a, price_b)
-
-@router.get("/price-coingecko")
-def price_goingecko(pool_name: str, token_pair: str):
-    return "price-coingecko"
-
-@router.get("/dex-screener")
-def price_dexscreener(pool_name: str, token_pair: str):
-    return "price_dexscreener"
